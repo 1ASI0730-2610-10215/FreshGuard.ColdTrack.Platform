c59d40075a0e2104f43c3be86aaf0a20ccf5426c
Mathias Arechaga <mathiasarechaga62@gmail.com>
2026-06-13 20:07:20 -0500

    feat(shipments): add shipment domain errors

---
+++ FreshGuard.ColdTrack.Platform/ShipmentManagement/Domain/Model/Errors/ShipmentError.cs
@@ -0,0 +1,9 @@
+namespace FreshGuard.ColdTrack.Platform.ShipmentManagement.Domain.Model.Errors;
+
+public enum ShipmentError
+{
+    ShipmentNotFound,
+    InvalidShipmentData,
+    InvalidShipmentStatus,
+    InvalidStatusTransition
+}
